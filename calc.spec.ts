4fbf2034534c63cbb24fee81c3a8cd4fb242fd41
MikeH211 <mikehanna1@yahoo.com>
2021-12-27 11:28:56 -0500

    Added files

---
+++ calc.spec.ts
@@ -0,0 +1,5 @@
+
+describe ("Calculator Tests", ()=>{
+
+    it("Should")
+})
---
+++ index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/add/:num1/:num2', () => {
+    
+})
